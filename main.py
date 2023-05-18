a0a393b538a06a62838beb777f8b133533282cff
Valiantsin <125600788+ChelGPT-5@users.noreply.github.com>
2023-05-18 16:48:17 +0300

    поправил переменную next -> next_step

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ summa = 0
 eps = 0.0001
 next_step = 1
 
-while abs(next) > eps:
-    next = pow(-1, n) * ((pow(x, 2 * n + 1)) / factorial(2 * n + 1))
-    summa += next
+while abs(next_step) > eps:
+    next_step = pow(-1, n) * ((pow(x, 2 * n + 1)) / factorial(2 * n + 1))
+    summa += next_step
     n += 1
 
 print(round(summa, 5))
