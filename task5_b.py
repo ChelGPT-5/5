810bd0526b23b786fbfcc73c45e0c9eee6923279
Valiantsin <125600788+ChelGPT-5@users.noreply.github.com>
2023-05-30 21:19:54 +0300

    Add files via upload

---
+++ task5_b.py
@@ -0,0 +1,14 @@
+from math import radians, factorial
+
+x = radians(20)
+n = 0
+summa = 0
+eps = 0.0001
+next_step = 1
+
+while abs(next_step) > eps:
+    next_step = pow(-1, n) * ((pow(x, 2 * n)) / factorial(2 * n))
+    summa += next_step
+    n += 1
+
+print(round(summa, 5))
